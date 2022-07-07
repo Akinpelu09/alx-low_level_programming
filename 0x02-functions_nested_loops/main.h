95cd97d24f074c48d1d538cf801fe893e4e49f60
Akinpelu09 <akinpeludaniel09@gmail.com>
2022-07-06 18:56:18 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef  MAIN_H                                                                                                                       
 #define  MAIN_H                                                                                                                       
-                                                                                                                             
+
+int print_last_digit(int);
 int _putchar (char c);                                                                                                                
 void print_alphabet(void);                                                                                                            
 void print_alphabet_x10(void);                                                                                                        
@@ -19,5 +20,5 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+
 #endif     
